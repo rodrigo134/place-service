3dd213bbc3037185c5bb8d05a4ac5f327e6766f7
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-10-27 16:21:40 -0300

    fix: configure R2DBC by updating pom.xml and adding schema.sql

--- src/main/java/com/rodrigo134/place_service/domain/PlaceRepository.java
+++ src/main/java/com/rodrigo134/place_service/domain/PlaceRepository.java
@@ -2,5 +2,7 @@ package com.rodrigo134.place_service.domain;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+
+
 public interface PlaceRepository extends ReactiveCrudRepository<Place, Long> {
 }
